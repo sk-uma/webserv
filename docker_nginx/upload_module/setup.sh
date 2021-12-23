08a04fe1fd0d2fa25fcbcc0a52737e0aed7e2d7f
TAKU1101 <taku.kuma1101@icloud.com>
2021-12-23 08:11:36 +0100

    Add: docker val: limit except

--- docker_nginx/upload_module/setup.sh
+++ docker_nginx/upload_module/setup.sh
@@ -1,3 +1,8 @@
+mkdir -p /var/www/html/{a..c}
+echo "a file" > /var/www/html/a/index.html
+echo "b file" > /var/www/html/b/index.html
+echo "c file" > /var/www/html/c/index.html
+
 service nginx start;
 service fcgiwrap start;
 
