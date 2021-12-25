ceaee3e7a2408c50ca2803eae985fa77b2999006
TAKU1101 <taku.kuma1101@icloud.com>
2021-12-25 06:38:18 +0100

    Add: docker nginx: val server_name

--- docker_nginx/upload_module/setup.sh
+++ docker_nginx/upload_module/setup.sh
@@ -3,6 +3,8 @@ echo "a file" > /var/www/html/a/index.html
 echo "b file" > /var/www/html/b/index.html
 echo "c file" > /var/www/html/c/index.html
 
+echo "127.0.0.1 hoge.net" >> /etc/hosts
+
 service nginx start;
 service fcgiwrap start;
 
